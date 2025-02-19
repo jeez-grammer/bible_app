baf9ca922ced475fc2048eee96dbceb0d052bab0
Nelson Jr. Viernes <nviernes@RISKCC-MBA-247.local>
2025-02-19 12:56:55 +0400

    Update Vercel Commit

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from app import create_app
+import logging
 
 app = create_app()
+
+# Add error handling
+@app.errorhandler(500)
+def internal_error(error):
+    logging.error(f"Server Error: {error}")
+    return "500 error", 500
+
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    logging.error(f"Unhandled Exception: {e}")
+    return "500 error", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
