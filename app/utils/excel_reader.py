81c62b27d94ce06bb23ce026242f601e57ae952f
Nelson Jr. Viernes <nviernes@RISKCC-MBA-247.local>
2025-02-19 13:36:32 +0400

    Update Vercel Commit

--- app/utils/excel_reader.py
+++ app/utils/excel_reader.py
@@ -4,9 +4,11 @@ import logging
 def get_excel_data():
     try:
         file_path = 'data/bible_app.xlsx'
+        logging.info(f"Attempting to read Excel file from {file_path}")
         df = pd.read_excel(file_path)
         df = df.sort_values(by='ID', ascending=False)
         data = df.to_dict(orient='records')
+        logging.info("Successfully read and processed Excel file")
         return data
     except Exception as e:
         logging.error(f"Error reading Excel file: {e}", exc_info=True)
